a07358fad155ae7601630bafa085ad7db06ea4f0
Xiaohai <804187225@qq.com>
2019-07-08 15:09:14 +0800

    添加一个自定义Drawable

--- customerview/src/main/java/com/demo/customerview/MainActivity.java
+++ customerview/src/main/java/com/demo/customerview/MainActivity.java
@@ -1,7 +1,11 @@
 package com.demo.customerview;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.demo.customerview.customer.CustomDrawable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img=findViewById(R.id.img);
+
+        CustomDrawable customDrawable=new CustomDrawable(Color.RED);
+        img.setBackground(customDrawable);
+
     }
 }
